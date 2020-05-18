4621538200d8a43a328d96f85ca7424858c43254
Math-Dzieu <dziedzic@et.esiea.fr>
2020-05-18 21:53:04 +0200

    New instagram_post.xml to display data

---
+++ app/src/main/java/com/example/instagramrecovery/InstagramPost.java
@@ -0,0 +1,14 @@
+package com.example.instagramrecovery;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InstagramPost extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.instagram_post);
+    }
+}
